7ed60674b214c7a08128c00afd407d14abbc77b4
Dani Surya Ramadhan <danisuryaramadhan@gmail.com>
2019-10-17 14:49:43 +0700

    Update Folder Model

---
+++ app/src/main/java/com/example/portalberita/model/ResponseBerita.java
@@ -0,0 +1,30 @@
+package com.example.portalberita.model;
+
+import java.util.List;
+import com.google.gson.annotations.SerializedName;
+
+
+public class ResponseBerita{
+
+	@SerializedName("data_berita")
+	private List<DataBeritaItem> dataBerita;
+
+	@SerializedName("status")
+	private boolean status;
+
+	public void setDataBerita(List<DataBeritaItem> dataBerita){
+		this.dataBerita = dataBerita;
+	}
+
+	public List<DataBeritaItem> getDataBerita(){
+		return dataBerita;
+	}
+
+	public void setStatus(boolean status){
+		this.status = status;
+	}
+
+	public boolean isStatus(){
+		return status;
+	}
+}
